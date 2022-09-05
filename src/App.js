0c5282b14c040d2dddde15dbaeb1c52a6d4c9b0c
felipe <fl.louzeiro@gmail.com>
2022-09-04 21:10:11 -0300

    criando formulário

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
 function App() {
   return (
-    <div>
-      <p>app</p>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className='containerInput'>
+        <input
+          type='text'
+          placeholder="Digite um CEP"
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color='#fff' />
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 98957000</h2>
+
+        <span>Rua Tarará</span>
+        <span>Complemento: Tarará</span>
+        <span>Bairro Tereré</span>
+        <span>São Paulo</span>
+      </main>
     </div>
   );
 }
